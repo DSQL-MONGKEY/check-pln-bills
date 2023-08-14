6e183f2ee7e64b379c67d1d74c0d7250bba5b9e9
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-15 06:20:34 +0700

    build<constans>: create constans file and navlinks export

---
+++ src/utils/constans.js
@@ -0,0 +1,17 @@
+const navLinks = [
+   {
+      title: 'About',
+      href: '#about'
+   },
+   {
+      title: 'Cek It',
+      href: '#cekit'
+   },
+   {
+      title: 'Support',
+      href: '#support'
+   }
+]
+
+
+export { navLinks }
