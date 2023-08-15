4440fb1b4fdf3d3a642a548bd45383b8dd98a94c
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-15 08:42:01 +0700

    feat<About> initial create about components

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import { moonlight } from "../assets"
+
+const About = () => {
+   return (
+      <section className="flex flex-col sm:flex-row justify-center items-center w-full p-5 h-[380px]">
+         <div>
+            <img src={moonlight} alt="about" className="w-[800px]" />
+         </div>
+         <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis eveniet ipsum impedit amet, itaque earum maxime, ullam culpa facilis error ab debitis molestiae doloribus optio recusandae deleniti neque. Ab.</p>
+         </div>
+      </section>
+   )
+}
+
+export default About
