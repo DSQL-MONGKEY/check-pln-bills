0fff23a26489a58b72100bf3823c85885771486d
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-15 09:07:11 +0700

    style<Navbar>edit backdrop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 
 const Navbar = () => {
    return (
-      <nav className="fixed flex justify-center top-0 w-full ">
+      <nav className="fixed flex z-20 justify-center top-0 w-full ">
          <div className="flex px-4 mt-2 gap-10 backdrop-blur-sm rounded-full border-2 border-slate-100">
             <a href="#about" className='p-2'>
                <HelpOutlineIcon/>
