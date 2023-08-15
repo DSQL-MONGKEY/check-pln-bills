5c6a0314088fc1d14cde0563e2501e65c885b6b9
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-15 08:41:07 +0700

    feat<Hero> create hero components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+   return (
+      <section className="w-full h-screen linear-white-gradient">
+         <div className="flex flex-col justify-center items-center gap-5 p-5 py-20 translate-y-10">
+            <img src="./logo.svg" alt="logo" width={200}/>
+         </div>
+      </section>
+   )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 const Navbar = () => {
    return (
       <nav className="fixed flex justify-center top-0 w-full ">
-         <div className="flex p-5 gap-5 ">
-            <a href="#about">
+         <div className="flex px-4 mt-2 gap-10 backdrop-blur-sm rounded-full border-2 border-slate-100">
+            <a href="#about" className='p-2'>
                <HelpOutlineIcon/>
             </a>
-            <a href="#cekit">
+            <a href="#cekit" className='p-2'>
                <ElectricBoltIcon/>
             </a>
-            <a href="#support">
+            <a href="#support" className='p-2'>
                <AllInclusiveIcon/>
             </a>
          </div>
