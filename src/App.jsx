93a1c7ce666bfb84a169ae3831c236ab7dbe6ac7
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-16 22:56:19 +0700

    feat<components>: add components to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-
+import About from "./components/About"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Service from "./components/Service"
 
 const App = () => {
   return (
-    <div className="">App</div>
+    <div className="app">
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Service/>
+    </div>
   )
 }
 
