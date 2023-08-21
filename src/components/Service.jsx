4d6b310be4ca74af0d4a1ef2d71615ac6159ed5c
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-21 13:47:32 +0700

    style<UI>: update the styles and layout

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,16 +1,11 @@
 import { useDebounce } from "@uidotdev/usehooks"
-import { useState, createContext, } from "react"
+import { useState } from "react"
 
-import { CircularProgress } from "@mui/material"
-import fetchAPI from "../utils/fetchAPI"
 import CardResult from "./CardResult"
 
-export const ServiceDataContext = createContext() 
 const Service = () => {
-   const [Idpel, setIdpel] = useState("")
-   const [isSearching, setIsSearching] = useState(true)
-   const [responseData, setResponseData] = useState([])
-   const debouncedIdpel = useDebounce(Idpel, 300)
+   const [idpel, setIdpel] = useState("")
+   const debouncedIdpel = useDebounce(idpel, 500)
 
 
    const handleChange = (e) => {
@@ -19,39 +14,34 @@ const Service = () => {
    const handleSubmit = (e) => {
       e.preventDefault()
       const formData = new FormData(e.target)
-      setIdpel(formData.get("search-id"))
-
-      if(debouncedIdpel) {
-         setIsSearching(true)
-         fetchAPI(Idpel)
-         .then(data => (
-            console.log(data.data),
-            setResponseData(data.data)
-         ))
-      }
-      setIsSearching(false)
+      setIdpel(formData.get("input-id"))
    }
 
+
    return (
-      <section id="cekit" className="w-full h-screen flex flex-col justify-center items-center gap-4">
-         <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col flex-wrap justify-center items-center gap-10">
-            <input
-               name="search-id"
-               type="number"
-               placeholder="Enter your ID"
-               onChange={(e) => handleChange(e)}
-               className="p-2 border-2 rounded-lg"
-            />
-            <button type="submit" disabled={isSearching} className="border-2 py-2 px-6 rounded-md" >
-               {!isSearching ? 
-                  <CircularProgress color="primary" /> : 
-                  'Search'
-               }
-            </button>
-         </form>
-         <ServiceDataContext.Provider value={{ responseData, isSearching }}>
-            <CardResult />
-         </ServiceDataContext.Provider>
+      <section id="cekit" className="w-full h-screen bg-[#FF2E63] p-10">
+         <div className="translate-y-20 flex flex-col sm:flex-row gap-5 sm:gap-10 text-white">
+            <h2 className="text-6xl sm:text-[100px] font-paytone">Cekit.</h2>
+            <p className="font-poppins">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eveniet velit deleniti voluptate maxime in quas eius quod, pariatur modi enim voluptatem voluptates qui nesciunt accusamus dolorum debitis a! Odit.
+            </p>
+         </div>
+         <div className="flex flex-col justify-center items-center gap-4 translate-y-20 mt-20">
+            <form onSubmit={handleSubmit} className="flex flex-col flex-wrap justify-center items-center gap-10 bg-slate-100">
+               <label htmlFor="input-id"></label>
+               <input
+                  name="input-id"
+                  type="text"
+                  placeholder="Enter your IDPEL"
+                  onChange={handleChange}
+                  className="p-2 border-2 rounded-lg"
+               />
+               <button type="submit" className="border-2 py-2 px-6 rounded-md" >
+                  SEARCH
+               </button>
+            </form>
+            <CardResult searchId={debouncedIdpel} />
+         </div>
       </section>
    )
 }
