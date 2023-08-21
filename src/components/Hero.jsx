6ef18b4e84cf5b0677d96583c729792a9c019a2b
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-21 11:46:12 +0700

    style<parallax>: add parallax effect to rectangle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,27 @@
 import { useParallax } from "react-scroll-parallax"
 
 const Hero = () => {
-   const { ref } = useParallax({ speed: 10, translateY: -20, })
+   const div = useParallax({ speed: -10, translateY: -20, scale: [1.5, 0.5]})
+   const text = useParallax({ speed: -10, translateY: -20, scale: [1.5, 0.5]})
+
+
    return (
       <section className="w-full h-screen linear-white-gradient">
-         <div className="flex flex-col justify-center  gap-5 p-5 py-20 ">
-            <div ref={ref} className="mt-10 text-center z-20">
+         <div ref={div.ref} className="flex flex-col justify-center gap-5 p-5 py-20">
+            <div ref={text.ref} className="mt-10 text-center translate-y-20 z-40">
                <h1>
                   <span className="text-3xl sm:text-7xl text-black font-poppins font-extrabold">The Alternative Way</span>
                </h1>
-               <p className="font-semibold text-cyan-600 font-">
+               <p className="font-semibold text-cyan-600 font-sanss">
                   To check your electricity bill 
                </p>
             </div>
-            <div className="flex justify-around translate-y-52 z-10">
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300"></div>
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300"></div>
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300"></div>
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300 hidden sm:block"></div>
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300 hidden sm:block"></div>
-               <div className="w-5 sm:w-10 h-5 sm:h-10 bg-blue-300 hidden sm:block"></div>
-               
+            <div className="flex justify-around mt-20">
+               <div className="w-10 h-10 bg-blue-300 sm:-translate-y-20"></div>
+               <div className="w-10 h-10 bg-blue-300"></div>
+               <div className="w-10 h-10 bg-blue-300 sm:-translate-y-10"></div>
+               <div className="w-10 h-10 bg-blue-300 hidden sm:block"></div>
+               <div className="w-10 h-10 bg-blue-300 hidden sm:block -translate-y-20"></div>
             </div>
          </div>
       </section>
