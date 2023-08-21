285dd7c5cd29ec1b689ac7368d6038d7abf7071c
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-21 14:01:39 +0700

    chore<lines>: remove blank lines

--- src/utils/fetchAPI.js
+++ src/utils/fetchAPI.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 const ENV_API_KEY = import.meta.env.VITE_API_KEY
-const BASE_URL = 'https://cek-id-pln-pasca-dan-pra-bayar.p.rapidapi.com/plnpostpaid'
+const ENV_BASE_URL = import.meta.env.VITE_BASE_URL
 
 const options = {
    method: 'GET',
-   url: BASE_URL,
+   url: ENV_BASE_URL,
    headers: {
       'X-RapidAPI-Key': ENV_API_KEY,
       'X-RapidAPI-Host': 'cek-id-pln-pasca-dan-pra-bayar.p.rapidapi.com'
@@ -15,7 +15,7 @@ const options = {
 
 const fetchAPI = async(idpel) => {
    try{
-      const { data } = await axios.get(`${BASE_URL}/${idpel}`, options)
+      const { data } = await axios.get(`${ENV_BASE_URL}/${idpel}`, options)
       return data
    }catch(err) {
       console.log(err.message)
