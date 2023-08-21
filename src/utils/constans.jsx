edaf4fb18b448c38546187aea6d349ff410090dc
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-21 13:59:32 +0700

    chore<cardContent>: add utils content card for about component

--- src/utils/constans.jsx
+++ src/utils/constans.jsx
@@ -3,23 +3,44 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 
+// Card Icon
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import WebhookIcon from '@mui/icons-material/Webhook';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+
 const navLinks = [
    {
-      title: 'about',
+      title: 'About',
       icon: <HelpOutlineIcon/>,
       href: '#about',
    },
    {
-      title: 'cekit',
+      title: 'Cek-it',
       icon:<ElectricBoltIcon/>, 
       href: '#cekit',
    },
    {
-      title: 'support',
+      title: 'Support',
       icon: <AllInclusiveIcon/>,
       href: '#support'
    }
 ]
 
-
-export { navLinks }
+const cardContent = [
+   {
+      icon: <QuestionMarkIcon/>,
+      sub: 'Alternative',
+      description: 'Born from personal problems that are always constrained and make it lazy enough to check electricity bills.'
+   },
+   {
+      icon: <WebhookIcon/>,
+      sub: 'Works',
+      description: 'Using API from PLN which is supported through the RapidAPI platform. Try our best to be able to help'
+   },
+   {
+      icon: <AutoGraphIcon/>,
+      sub: 'Limit',
+      description: 'Because the API is not paid, the functionality on this web has a limit of 25 requests per day and not individuals'
+   },
+]
+export { navLinks, cardContent }
