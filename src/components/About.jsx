f5af54fffdb1c33019d75a146498208a76cdf6a9
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-21 13:54:25 +0700

    style<border>: add border style to the card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,34 @@
-import { moonlight } from "../assets"
+import { Avatar, Card, CardContent, Typography } from "@mui/material"
+import { cardContent } from "../utils/constans"
 
 const About = () => {
    return (
-      <section id="about" className="flex flex-col sm:flex-row justify-center items-center w-full p-5 h-[380px]">
-         <div>
-            <img src={moonlight} alt="about" className="w-[800px]" />
-         </div>
-         <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis eveniet ipsum impedit amet, itaque earum maxime, ullam culpa facilis error ab debitis molestiae doloribus optio recusandae deleniti neque. Ab.</p>
-         </div>
+      <section id="about" className="flex flex-col sm:flex-row justify-center items-center w-full p-5 h-[380px] gap-5 z-50">
+         {
+            cardContent.map(content => (
+               <Card key={content.sub} outlined="true" 
+               sx={{
+                  display: 'flex', 
+                  flexDirection: 'column', 
+                  alignItems: 'center', 
+                  paddingY: '10px',
+                  maxWidth: '380px', 
+                  minHeight: { xs:'160px', md:'200px' },
+                  borderBottom: 5,
+                  borderRadius: 2,
+                  borderBottomColor: '#40F8FF'
+                  }}>
+                     <Avatar sx={{ bgcolor: '#FF2E63' }}>
+                        {content.icon}
+                     </Avatar>
+                     <CardContent>
+                        <Typography>
+                           {content.description}
+                        </Typography>
+                     </CardContent>
+               </Card>
+            ))
+         }
       </section>
    )
 }
