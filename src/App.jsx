671d98068454670c41a0f50b1d1408f9afa5fe0a
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-24 10:25:17 +0700

    feat<Credit>: add credit component to be render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Credit from "./components/Credit"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Service from "./components/Service"
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Service/>
+      <Credit/>
     </div>
   )
 }
