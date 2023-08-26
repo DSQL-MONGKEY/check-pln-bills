ae1d08872043782eaea980228c7692a02e999ae7
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-26 22:45:13 +0700

    refactor<Result>:change fetch api to the component and add moda error component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,10 @@
 import { useEffect, useState } from "react";
+import PropTypes from 'prop-types'
+import axios from "axios";
+
 
 // List item material UI
-import {
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Avatar,
-} from "@mui/material"
-import PropTypes from 'prop-types'
+import { Skeleton } from "@mui/material"
 
 // icons
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
@@ -16,51 +12,69 @@ import NumbersIcon from '@mui/icons-material/Numbers';
 import PersonIcon from '@mui/icons-material/Person';
 import BoltIcon from '@mui/icons-material/Bolt';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import MessageIcon from '@mui/icons-material/Message';
 
+import { options, ENV_BASE_URL } from "../utils/options";
+import Modal from "./Modal";
 
-import fetchAPI from "../utils/fetchAPI";
 
 const Result = ({ searchId }) => {
   const [responseData, setResponseData] = useState([])
-
-  useEffect(() => {
-    if(searchId) {
-      fetchAPI(searchId)
-      .then(data => setResponseData(data?.data))
-      .catch(err => console.log(err.err))
-    }
-  }, [searchId])
-
+  const [isSearhing, setIsSearching] = useState(false)
+  const [error, setError] = useState(false)
+  
   let indonesianRupiah = Intl.NumberFormat('en-ID', {
     style: 'currency',
     currency: 'IDR'
   })
 
-  if(responseData.subscriber_name == undefined) return (
-    <List 
-    sx={{ 
-      width: '100%',
-      maxWidth: 500,
-      bgcolor: 'white',
-      borderRadius: 5,
-      border: 4,
-      borderColor: 'rgb(226 232 240)'
-    }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar sx={{ bgcolor: '#FF2E63' }}>
-            <ReportProblemIcon/>  
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText 
-        primary={responseData} 
-        />
-      </ListItem>
-    </List>
+  const fetchData = async(searchId) => {
+    try {
+      const { data } = await axios.get(`${ENV_BASE_URL}/${searchId}`, options)
+      return data
+    }catch(err) {
+      console.log(err)
+      if(err.response.status === 429) {
+        setError(true)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if(searchId) {
+      setIsSearching(true)
+      fetchData(searchId)
+      .then(res => setResponseData(res?.data))
+      .then(setIsSearching(false))
+    }
+    setError(false)
+  }, [responseData, searchId])
+  console.log(error)
+  if(error) return <Modal/>
+
+  if(responseData?.subscriber_name == undefined) return (
+    <div className={`container bg-white w-[350px] md:w-[550px] rounded-xl border-4 border-slate-200 p-3`}>
+      <div className="flex gap-3 items-center px-1 py-2">
+        <span className="bg-[#FF2E63] text-center text-white py-2.5 px-3 rounded-full">
+          <MessageIcon/>
+        </span>
+        <div>
+          {isSearhing ?
+            <Skeleton variant="text" width={400} height={20}/>
+          :
+            <p className="text-zinc-500 text-md sm:text-xl font-outfit">
+              {responseData !== undefined ? responseData : 
+              'Data kamu akan ditampilkan disini'}
+            </p>
+          }
+        </div>
+      </div>
+    </div>
   )
 
+
   return (
-    <div className="container bg-white w-5/10 md:w-1/2 rounded-xl border-4 border-slate-200 p-4">
+    <div className="container bg-white w-[350px] md:w-[550px] rounded-xl border-4 border-slate-200 p-3">
       <div className="flex flex-col flex-wrap gap-3 font-sans">
         <div className="flex gap-3 px-1 py-2">
             <span className="bg-[#FF2E63] text-center text-white py-2.5 px-3 rounded-full"><NumbersIcon/></span>
